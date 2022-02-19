b3f5570cd009c57848e49ba9bc38251e49c36f5c
Chris Wright <chris@dxw.com>
2022-02-19 14:32:51 +0000

    Add main.tf
    * It's empty, but is usually the main entry point
    * Contains list of terraform files and their function

---
+++ main.tf
@@ -0,0 +1,8 @@
+# https://github.com/chris-qa-org/terraform-aws-organzation-and-sso
+#
+# main terraform files:
+# org.tf: AWS Organizaion resources
+# sso.tf: AWS Single Sign On resources
+# locals.tf: local values
+# variables.tf: Input variables
+# outputs.tf: Output values
